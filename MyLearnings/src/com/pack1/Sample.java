efaad426df32d2a0bd98b3df185120ed6bb3ecac
VishalJana1999 <jana.vishal9025@gmail.com>
2022-10-18 17:05:28 +0530

    Removed the programs from new branch which are from master

--- MyLearnings/src/com/pack1/Sample.java
+++
@@ -1,8 +0,0 @@
-package com.pack1;
-
-public class Sample {
-	public static void main(String[] args) {
-		AccessModifiers am = new AccessModifiers();
-		System.out.println(am.i);
-	}
-}
--- MyLearnings/src/com/pack1/SampleSubClass.java
+++
@@ -1,8 +0,0 @@
-package com.pack1;
-
-public class SampleSubClass extends AccessModifiers {
-	public static void main(String[] args) {
-		SampleSubClass sc = new SampleSubClass();
-		System.out.println(sc.i);
-	}
-}
